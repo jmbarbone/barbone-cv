cb39dc70886131b55ce2ac86332731feb11bcdb3
Jordan Mark Barbone <jmbarbone@gmail.com>
2024-09-09 21:16:02 -0400

    restructure

--- R/utils.R
+++ R/utils.R
@@ -1,14 +1,17 @@
 
 with_cleanup <- function(dir, code) {
+  wd <- getwd()
   old <- fs::dir_ls()
   try(force(code))
-  new <- setdiff(fs::dir_ls(), old)
-  # ignore the CV and Resume files
-  new <- grep("Barbone-(CV|Resume)\\.pdf", new, invert = TRUE, value = TRUE)
-  cat("Moving new files", paste("\n-", new, collapse = ", "), "\n")
-  fs::dir_create(dir)
-  fs::file_move(new, fs::path(dir, new))
+  withr::with_dir(wd, {
+    new <- setdiff(fs::dir_ls(), old)
+    # ignore the CV and Resume files
+    new <- new[grep("$Barbone-(CV|Resume)\\.pdf$", basename(new), invert = TRUE)]
+    cat("Moving new files", paste("\n-", new, collapse = ", "), "\n")
+    fs::dir_create(dir)
+    fs::file_move(new, fs::path(dir, new))
 
-  known <- basename(fs::dir_ls(dir))
-  fs::file_delete(known[fs::file_exists(known)])
+    known <- basename(fs::dir_ls(dir))
+    fs::file_delete(known[fs::file_exists(known)])
+  })
 }
