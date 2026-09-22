8a1a8faf4f0a52ee8cc43f8de86b60be29030416
Jordan Mark Barbone <jmbarbone@gmail.com>
2026-09-21 21:33:03 -0400

    cleans up packages

--- R/packages.R
+++ R/packages.R
@@ -1,14 +1,25 @@
 update_cv_library <- function() {
-  packages <- subset(
-    tools::CRAN_package_db(),
-    grepl("jmbarbone@gmail.com", `Authors@R`, fixed = TRUE),
-    "Package",
-    drop = TRUE
-  )
-  
+  i_am_author <- function(x) {
+    its_me <- function(p) p$email == "jmbarbone@gmail.com" && "aut" %in% p$role
+    any(vapply(x, its_me, NA))
+  }
+
+  packages <-
+    tools::CRAN_package_db() |>
+    subset(!is.na(`Authors@R`)) |>
+    subset(grepl("Barbone", `Authors@R`, fixed = TRUE)) |>
+    transform(
+      `Authors@R` = lapply(`Authors@R`, \(i) eval(str2expression(i)))
+    ) |>
+    subset(
+      vapply(`Authors@R`, i_am_author, NA),
+      "Package",
+      drop = TRUE
+    )
+
   lib <- Sys.getenv("R_LIB_CV", "~/R/cv-library")
   fs::dir_create(lib, ", ")
-  
+
   pak::pak(packages, lib = lib, ask = FALSE)
 
   knitr::write_bib(
